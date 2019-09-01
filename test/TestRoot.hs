766234656ef925d2c3fc55e4640d1b0d8d0c26a4
Gary Haussmann <gjhaussmann@gmail.com>
2019-08-31 22:19:09 -0600

    Basic working test for ReplaceOnly

--- test/TestRoot.hs
+++ test/TestRoot.hs
@@ -1,19 +1,69 @@
-module Main (main) where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-import System.Exit (exitFailure, exitSuccess)
-import Test.QuickCheck
-import Test.HUnit
---import Zorja.Patchable
+module Main (
+    main
+    ) where
 
-test1 :: Test
-test1 = TestCase (assertEqual "for (1+2)," (3 :: Integer) (1+2))
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 
-testList :: Test
-testList = TestList [test1]
 
-main :: IO b
-main = do
-    (Counts _caseCount _tryCount errCount failCount) <- runTestTT testList
-    if (errCount + failCount == 0)
-        then exitSuccess
-        else exitFailure
+import Zorja.Patchable
+import Zorja.Primitives
+
+--
+-- | Patch generator: can generate a value of type 'a' which is 'Patchable'
+--  and also given an 'a' can generate a 'PatchDelta a' value for that
+--  value
+--
+data PatchableGen a = PatchableGen
+    {
+        genValue :: Gen a,
+        genDelta :: a -> Gen (PatchDelta a)
+    }
+
+--
+-- | Generator for the ReplaceOnly primitive.  Given a value generator
+--  this will produce something to generate values and deltas.
+--
+replaceOnlyGenerator :: Gen a -> PatchableGen (ReplaceOnly a)
+replaceOnlyGenerator g =
+    PatchableGen
+    {
+        genValue = fmap ReplaceOnly g,
+        genDelta = \_ -> Gen.frequency
+            [
+                (1, (Gen.constant $ Replacing Nothing)),
+                (3, fmap (Replacing . Just) g)
+            ]
+    }
+
+
+patchmerge_onetype :: 
+    (Eq a, Show a, Show (PatchDelta a), Patchable a)
+        => PatchableGen a -> PropertyT IO ()
+patchmerge_onetype (PatchableGen g dg) =
+    do
+        x   <- forAll g
+        dx1 <- forAll (dg x)
+        dx2 <- forAll (dg (patch x dx1))
+        (patch (patch x dx1) dx2) === (patch x (mergepatches dx1 dx2))
+
+prop_patchmerge :: Property
+prop_patchmerge = 
+    property $ do
+        let intgen   = Gen.integral $ Range.linear (0::Integer) (100::Integer)
+        let floatgen = Gen.float $ Range.linearFrac (0.0::Float) (100.0::Float)
+        patchmerge_onetype (replaceOnlyGenerator intgen)
+        patchmerge_onetype (replaceOnlyGenerator floatgen)
+        patchmerge_onetype (replaceOnlyGenerator Gen.bool)
+
+
+tests :: IO Bool
+tests = checkSequential $$(discover)
+
+main :: IO Bool
+main = tests
